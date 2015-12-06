91b294890644293090eaac927c760ef1723578e5
Baptiste Mispelon <bmispelon@gmail.com>
2015-12-06 12:02:28 +0100

    Made proposal form in admin readonly

--- cfp/admin.py
+++ cfp/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
+from django import forms
 
 from .models import Proposal
+
+
+class ProposalReadOnlyForm(forms.ModelForm):
+    class Meta:
+        model = Proposal
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(ProposalReadOnlyForm, self).__init__(*args, **kwargs)
+        for f in self.fields.values():
+            if f.widget.is_hidden:
+                continue
+            f.widget.attrs.setdefault('readonly', True)
 
 
 @admin.register(Proposal)
 class ProposalAdmin(admin.ModelAdmin):
     exclude = ['name', 'email', 'speaker_information']
+    form = ProposalReadOnlyForm
